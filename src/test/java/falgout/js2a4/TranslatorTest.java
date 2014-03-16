f29333bda133f80845691f6e7f6b0a4497755cdd
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-16 14:24:24 -0400

    Added even more progress information

--- src/test/java/falgout/js2a4/TranslatorTest.java
+++ src/test/java/falgout/js2a4/TranslatorTest.java
@@ -1,36 +1,50 @@
 package falgout.js2a4;
 
+import static org.junit.Assert.fail;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.atn.PredictionMode;
 import org.junit.Test;
 
 public class TranslatorTest {
     @Test
-    public void translationCanParseJDK() throws IOException, InterruptedException, ExecutionException {
+    public void translationCanParseJDK() throws IOException, InterruptedException {
         ClassLoader cl = getClass().getClassLoader();
         
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         try (ZipInputStream src = new ZipInputStream(cl.getResourceAsStream("src.zip"))) {
-            ZipEntry e;
-            byte[] buf = new byte[1024 * 8];
-            ExecutorService runner = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-            Queue<Future<String>> tasks = new ArrayDeque<>();
+            ExecutorCompletionService<Void> runner = new ExecutorCompletionService<>(executor);
+            
             final AtomicInteger total = new AtomicInteger();
             final AtomicInteger complete = new AtomicInteger();
+            final AtomicLong totalTime = new AtomicLong();
             
+            Map<Future<Void>, String> tasks = new LinkedHashMap<>();
+            final Set<String> processing = new CopyOnWriteArraySet<>();
+            
+            byte[] buf = new byte[1024 * 8];
+            ZipEntry e;
             while ((e = src.getNextEntry()) != null) {
                 final String name = e.getName();
                 if (name.endsWith(".java")) {
@@ -40,29 +54,54 @@ public class TranslatorTest {
                         sink.write(buf, 0, read);
                     }
                     final String file = new String(sink.toByteArray());
-                    tasks.add(runner.submit(new Callable<String>() {
+                    tasks.put(runner.submit(new Callable<Void>() {
                         @Override
-                        public String call() throws Exception {
+                        public Void call() throws Exception {
                             JavaLexer lex = new JavaLexer(new ANTLRInputStream(file));
-                            JavaParser2 parse = new JavaParser2(new CommonTokenStream(lex));
+                            JavaParser3 parse = new JavaParser3(new CommonTokenStream(lex));
+                            
+                            parse.getInterpreter().setPredictionMode(PredictionMode.SLL);
                             parse.setErrorHandler(new BailErrorStrategy());
-
-                            System.out.println("Starting              " + name);
+                            
+                            processing.add(name);
+                            long nanos = System.nanoTime();
                             parse.compilationUnit();
+                            nanos = System.nanoTime() - nanos;
+                            processing.remove(name);
+                            totalTime.addAndGet(nanos);
                             int c = complete.incrementAndGet();
                             int t = total.get();
                             double percent = 100 * c / (double) t;
-                            System.out.printf("Done (%d/%d : %.2f%%) %s\n", c, t, percent, name);
-                            return name;
+                            double seconds = nanos / 10e9;
+                            System.out.printf("Done (%d/%d : %.2f%%) %.2f seconds %s\n", c, t, percent, seconds, name);
+                            return null;
                         }
-                    }));
+                    }), name);
                     total.incrementAndGet();
                 }
             }
             
-            for (Future<String> f : tasks) {
-                f.get();
+            List<String> failed = new ArrayList<>();
+            int taken = 0;
+            while (taken < total.get()) {
+                Future<Void> f = runner.take();
+                taken++;
+                try {
+                    f.get();
+                } catch (ExecutionException e1) {
+                    failed.add(tasks.get(f));
+                    System.err.println("Failed " + tasks.get(f));
+                }
             }
+            
+            System.out.printf("Done in %.2f seconds\n", totalTime.get() / 10e9);
+            
+            if (failed.size() > 0) {
+                System.out.println(failed);
+                fail(failed.toString());
+            }
+        } finally {
+            executor.shutdownNow();
         }
     }
 }
